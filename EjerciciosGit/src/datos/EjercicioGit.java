11161c5db69a5b591afabb92f93145e173d6c248
csi1-FJavier <csi1-FJavier@FJBR>
2017-10-05 13:30:50 +0200

    syso de cambios

--- EjerciciosGit/src/datos/EjercicioGit.java
+++ EjerciciosGit/src/datos/EjercicioGit.java
@@ -3,7 +3,7 @@ package datos;
 public class EjercicioGit {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("He modificado el fichero");
 
 	}
 
