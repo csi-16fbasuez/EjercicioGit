092779a5ef04c66a0639f32b952978a928fc469f
csi1-FJavier <csi1-FJavier@FJBR>
2017-10-05 13:06:38 +0200

    Mensaje de cambios en el proyecto

---
+++ EjercicioGit/src/datos/Principal.java
@@ -0,0 +1,14 @@
+package datos;
+
+import java.util.Scanner;
+
+public class Principal {
+
+	private static Scanner sc=new Scanner(System.in);
+	public static void main(String[] args) {
+	
+
+		sc.close();
+	}
+
+}
